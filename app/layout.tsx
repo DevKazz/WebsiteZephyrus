b73ef4a446ee33419a763619f6476ed98b5e816f
kaz <ka.zi.fnn.9@gmail.com>
2024-05-23 22:43:42 +0300

    dply

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons: { 
+    icon: '/favicon.ico'
+  },
   title: "Zephyrus",
   description: "Team of developers",
 };
