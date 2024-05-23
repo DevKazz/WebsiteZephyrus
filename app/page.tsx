a55d53e4929a145c05d206ad36050562625b9338
kaz <ka.zi.fnn.9@gmail.com>
2024-05-23 22:49:50 +0300

    dply

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 import { Hero } from "@/app/landing/Hero"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Zephyrus",
+  description: "Team Of Developers",
+  // ... (other metadata properties like icons, Open Graph tags, etc.)
+};
+
 
 export default function Home() {
   return (
